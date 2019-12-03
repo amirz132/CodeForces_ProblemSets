13ad83eec6cf6ccf0f50a365535fc1f49c9d53f1
amirz132 <46897821+amirz132@users.noreply.github.com>
2019-12-03 15:09:14 -0500

    Create TheatreSquare.java

---
+++ TheatreSquare.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ 
+public class TheatreSquare {
+    
+    static Scanner sr = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+        double n = sr.nextInt();
+        double m = sr.nextInt();
+        double a = sr.nextInt();
+        
+        System.out.println((long)(Math.ceil(n/a) * Math.ceil(m/a)));
+    }
+}
